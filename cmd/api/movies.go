312729bc4fba45c4eabf7d539c5bb8972c65f9d7
Alikhan <alikhanaitbayevpost@gmail.com>
2023-05-27 15:41:37 +0600

    next

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -173,5 +173,14 @@ func (app *application) listMovieSHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
