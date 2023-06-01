e4da3ea6a1540ec538189bc55369030d875d6565
Alikhan <alikhanaitbayevpost@gmail.com>
2023-06-01 14:17:31 +0600

    next

--- down.sql
+++ down.sql
@@ -6,4 +6,8 @@ ALTER TABLE movies DROP CONSTRAINT IF EXISTS movies_year_check;
 ALTER TABLE movies DROP CONSTRAINT IF EXISTS genres_length_check;
 -- 2
 
+DROP INDEX IF EXISTS movies_title_idx;
+DROP INDEX IF EXISTS movies_genres_idx;
+-- 3 
+
 
--- up.sql
+++ up.sql
@@ -14,3 +14,7 @@ ALTER TABLE movies ADD CONSTRAINT movies_runtime_check CHECK (runtime >= 0);
 ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 1888 AND date_part('year', now()));
 ALTER TABLE movies ADD CONSTRAINT genres_length_check CHECK (array_length(genres, 1) BETWEEN 1 AND 5);
 -- 2
+
+CREATE INDEX IF NOT EXISTS movies_title_idx ON movies USING GIN (to_tsvector('simple', title));
+CREATE INDEX IF NOT EXISTS movies_genres_idx ON movies USING GIN (genres);
+-- 3
