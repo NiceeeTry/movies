ed6c2ab891f17204162bc2e294fa0fa1ae0e1e40
Alikhan <alikhanaitbayevpost@gmail.com>
2023-06-14 10:36:25 +0600

    user model database query

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"rest_api/internal/validator"
 	"time"
@@ -76,3 +78,35 @@ func ValidateUser(v *validator.Validator, user *User) {
 		panic("missing password hash for user")
 	}
 }
+
+var (
+	ErrDuplicateEmail = errors.New("duplicate email")
+)
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m UserModel) Insert(user *User) error {
+	query := `INSERT INTO users (name, email, password_hash, activated)
+			VALUES ($1, $2, $3, $4)
+			RETURNING id,created_at, version`
+	args := []interface{}{user.Name, user.Email, user.Password.hash, user.Activated}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
+
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+///284 page
